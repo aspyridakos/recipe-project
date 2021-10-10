3ab16d1749d30febf8d7c8de1cd0332027a7947d
Alexandra <aspyridakos@gmail.com>
2021-10-09 20:51:18 -0400

    linked up navbar to pages and started shopping list edit properties

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,9 +7,11 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://www.halfbakedharvest.com/wp-content/uploads/2021/09/Sheet-Pan-Sticky-Sweet-and-Sour-Chicken-1-340x510.jpg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.halfbakedharvest.com/wp-content/uploads/2021/09/Sheet-Pan-Sticky-Sweet-and-Sour-Chicken-1-340x510.jpg')
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://www.halfbakedharvest.com/wp-content/uploads/2021/09/Sheet-Pan-Sticky-Sweet-and-Sour-Chicken-1-340x510.jpg')
   ];
 
   constructor() { }
@@ -17,5 +19,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
-
+  onRecipeSelected(recipe:Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
